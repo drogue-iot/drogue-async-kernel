edc5228e09bbf7db8cf8435e2eeb1e0d5679e9f8
Bob McWhirter <bmcwhirt@redhat.com>
2020-11-24 15:54:53 -0500

    Add some rustdocs.

--- src/actor.rs
+++ src/actor.rs
@@ -1,15 +1,38 @@
 use crate::event::Event;
 use bare_metal::Nr;
 
+/// A live component of the system. May be written in a re-usable style.
+///
+/// Given the non-HALness of actual interrupt setting and clearing, an
+/// `Actor` may be paired with an `InterruptHandler<_,_>` for a complete,
+/// interrupt-driven component.
 pub trait Actor {
+
+    /// The type of event this actor work in terms of.
     type Event;
+
+    /// Process an event, possibly doing nothing.
     fn process(&mut self, _event: Event<Self::Event>) {}
 
+    /// Handle an associated interrupt.
     fn interrupt(&mut self) {}
 }
 
+/// A trait which may be associated (but not implemented by)
+/// an actor, in order to perform platform-specific interrupt
+/// checking and clearing.
 pub trait InterruptHandler<C: Actor, Irq: Nr + Copy> {
+
+    /// The interrupt line associated with this handler.
+    /// Given that several semantic interrupts may be linked
+    /// to a given line (EXTI for instance), further checking
+    /// will be performed by `check_interrupt(...)`.
     const IRQ: Irq;
+
+    /// Check if the interrupt line actually desires to
+    /// operate *this* particular action.
     fn check_interrupt(component: &mut C) -> bool;
+
+    /// Clear the interrupt for this particular action.
     fn clear_interrupt(component: &mut C);
 }
--- src/event.rs
+++ src/event.rs
@@ -1,17 +1,32 @@
 use core::fmt::Debug;
 
+
+/// Marker trait to allow for application events to denote a `None` variant
+/// since orphan rules prevent `Into`/`From` for `Option<T>` even if `T` is
+/// local to the crate.
 pub trait Optional {
+    /// Determine if this event variant is equivalent to `None`
     fn is_none(&self) -> bool;
 }
 
+
+/// An event to be handled.
+///
+/// The `Kernel` variant is only creatable by the kernel
+/// itself and denotes lifecycle events.
+///
+/// The `Actor` variant will wrap an application or actor-specific
+/// event for ingress or egress.
 #[derive(Debug, Copy, Clone)]
 pub enum Event<E> {
     Kernel(KernelEvent),
     Actor(E),
 }
 
+/// A kernel lifecycle event.
 #[derive(Debug, Copy, Clone)]
 pub enum KernelEvent {
+    /// Initialization prior to interrupts or the first event-loop.
     Initialize,
     Start,
     Shutdown,
